66aded49c71cfb81eb9b2be3910e2d26bd7aed46
StabbyCutyou <smattkelly@gmail.com>
2019-03-03 15:34:40 -0500

    Adding in new example for timings in sample app

--- cmd/sample/main.go
+++ cmd/sample/main.go
@@ -9,10 +9,12 @@ import (
 )
 
 func main() {
-	ctx := timing.NewContext(context.Background())
+	ctx := timing.WithTiming(context.Background())
 	FunctionA(ctx)
 	spew.Dump(ctx.Timings())
-
+	ctx = timing.WithTiming(context.Background())
+	FunctionSlowChain(ctx)
+	spew.Dump(ctx.Timings())
 }
 
 // FunctionA is a dummy
@@ -33,4 +35,36 @@ func FunctionB(ctx timing.Context) {
 func FunctionC(ctx timing.Context) {
 	defer ctx.Start().Stop()
 	time.Sleep(50 * time.Millisecond)
+}
+
+func FunctionSlowChain(ctx timing.Context) {
+	defer ctx.Start().Stop()
+	time.Sleep(1 * time.Millisecond)
+	link1(ctx)
+}
+
+func link1(ctx timing.Context) {
+	defer ctx.Start().Stop()
+	time.Sleep(10 * time.Millisecond)
+	link2(ctx)
+}
+
+func link2(ctx timing.Context) {
+	defer ctx.Start().Stop()
+	time.Sleep(100 * time.Millisecond)
+	link3(ctx)
+	link3(ctx)
+	link3(ctx)
+}
+
+func link3(ctx timing.Context) {
+	defer ctx.Start().Stop()
+	time.Sleep(500 * time.Millisecond)
+	link4(ctx)
+	link4(ctx)
+}
+
+func link4(ctx timing.Context) {
+	defer ctx.Start().Stop()
+	time.Sleep(1000 * time.Millisecond)
 }
