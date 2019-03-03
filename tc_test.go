81978a9d5fbb689f07fd043f9401790a5b93c6e1
StabbyCutyou <smattkelly@gmail.com>
2019-03-03 15:34:25 -0500

    Fixing calculation in timings, testing and supporting WithoutTiming

--- tc_test.go
+++ tc_test.go
@@ -5,23 +5,39 @@ import (
 	"testing"
 )
 
+func getCallerPC() string {
+	return getCallerFuncPC(2)
+}
+
 func BenchmarkPC(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		getCallerPC()
 	}
 }
 
-func BenchmarkTrace(b *testing.B) {
-	ctx := NewContext(context.Background())
+func BenchmarkWithTiming(b *testing.B) {
+	ctx := WithTiming(context.Background())
 	for i := 0; i < b.N; i++ {
 		traceMyStack(ctx)
 	}
 }
 
-func getCallerPC() string {
-	return getCallerFuncPC(2)
+func BenchmarkWithoutTiming(b *testing.B) {
+	ctx := WithoutTiming(context.Background())
+	for i := 0; i < b.N; i++ {
+		traceMyStack(ctx)
+	}
 }
 
 func traceMyStack(ctx Context) {
 	defer ctx.Start().Stop()
+}
+
+func TestWithoutTiming(t *testing.T) {
+	ctx := WithoutTiming(context.Background())
+	ctx.Start()
+	ctx.Stop()
+	if ctx.Timings() != nil {
+		t.Fatal("timing dict should have been nil, is not")
+	}
 }
